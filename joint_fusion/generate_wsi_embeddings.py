b31531f79765be79d7d2b036021eed73e6fa8985
tnnandi <tnnandi@gmail.com>
2024-02-16 01:40:20 -0600

    pulling lunit-DINO for WSI feature extraction

---
+++ joint_fusion/generate_wsi_embeddings.py
@@ -0,0 +1,46 @@
+# train models, or use pretrained models to generate WSI embeddings
+from pdb import set_trace
+import torch
+from timm.models.vision_transformer import VisionTransformer
+
+# Load the png images and their corresponding censoring data and survival time
+
+
+
+# Create the dataloader
+
+
+
+
+# Use the pretrained Lunit-DINO model for WSI feature extraction (trained on histopathology images)
+# LUnit-DINO uses the ViT-S architecture with DINO for SSL
+
+def get_pretrained_url(key):
+    URL_PREFIX = "https://github.com/lunit-io/benchmark-ssl-pathology/releases/download/pretrained-weights"
+    model_zoo_registry = {
+        "DINO_p16": "dino_vit_small_patch16_ep200.torch",
+        "DINO_p8": "dino_vit_small_patch8_ep200.torch",
+    }
+    pretrained_url = f"{URL_PREFIX}/{model_zoo_registry.get(key)}"
+    return pretrained_url
+
+
+def vit_small(pretrained, progress, key, **kwargs):
+    patch_size = kwargs.get("patch_size", 16)
+    model = VisionTransformer(
+        img_size=224, patch_size=patch_size, embed_dim=384, num_heads=6, num_classes=0
+    )
+    if pretrained:
+        pretrained_url = get_pretrained_url(key)
+        verbose = model.load_state_dict(
+            torch.hub.load_state_dict_from_url(pretrained_url, progress=progress)
+        )
+        print(verbose)
+    return model
+
+
+if __name__ == "__main__":
+    # initialize ViT-S/16 trunk using DINO pre-trained weight
+    model = vit_small(pretrained=True, progress=False, key="DINO_p16", patch_size=16)
+
+
