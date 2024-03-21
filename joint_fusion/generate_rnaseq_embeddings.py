b4fd30f30c9b512ab1c9da1f095cc9ab9a98c7b9
tnnandi <tnnandi@gmail.com>
2024-03-21 11:46:46 -0500

    minor changes for running a baseline VAE training

--- joint_fusion/generate_rnaseq_embeddings.py
+++ joint_fusion/generate_rnaseq_embeddings.py
@@ -1,9 +1,12 @@
 # Implement a MLP based VAE to create lower dimensional embeddings of the RNASeq data
+# conda activate pytorch_py3p10 on laptop
 # use 'wandb sweep sweep_config.yaml' followed by 'wandb agent <username>/<project_name>/<sweep_id>' [just follow the instructions after 'wandb sweep'] for HPO runs with wandb
+# for running without HPO, simply use "python generate_rnaseq_embeddings.py"
 
 import torch
 from torch import nn, optim
 import copy
+import os
 from torch.optim.lr_scheduler import CosineAnnealingLR
 from torch.utils.data import TensorDataset, DataLoader
 from torchsummary import summary
@@ -28,9 +31,12 @@ else:
 # rnaseq_df = pd.read_csv(
 #     "/mnt/c/Users/tnandi/Downloads/multimodal_lucid/multimodal_lucid/preprocessing/combined_rnaseq_TCGA-LUAD.csv" , delimiter='\t'
 # )
-rnaseq_df=pd.read_csv(
-    "/mnt/c/Users/tnandi/Downloads/multimodal_lucid/multimodal_lucid/preprocessing/batchcorrected_combined_rnaseq_TCGA-LUAD.tsv", delimiter='\t'
+rnaseq_df = pd.read_csv(
+    "/mnt/c/Users/tnandi/Downloads/multimodal_lucid/multimodal_lucid/preprocessing/batchcorrected_combined_rnaseq_TCGA-LUAD.tsv",
+    delimiter='\t'
 )
+checkpoint_dir = "./checkpoints/"
+os.makedirs(checkpoint_dir, exist_ok=True)
 
 # Remove the code below when using shortened TCGA IDs when creating the combined RNASeq file
 # rename the columns by keeping only the minimal part of the TCGA ID, e.g.,  TCGA-44-2655
@@ -52,16 +58,18 @@ print("Are all the gene_names unique: ", rnaseq_df.shape[0] == len(rnaseq_df['ge
 # model parameters
 # use rnacdm's parameters
 input_dim = rnaseq_df.shape[0]  # number of genes (~ 20K)
-# config.intermediate_dim = 512
-intermediate_dim = config.intermediate_dim
-lr = config.learning_rate
+
+# intermediate_dim = config.intermediate_dim
+# lr = config.learning_rate
+intermediate_dim = 512
+lr = 1e-4
+
 latent_dim = 256
-beta = 0 #0.005 #0.01   # 0: equivalent to standard autoencoder; 1: equiavlent to standard VAE
-# lr = 1e-3
+beta = 0.005  # 0.01   # 0: equivalent to standard autoencoder; 1: equiavlent to standard VAE
 train_batch_size = 128
 val_batch_size = 8
 test_batch_size = 8
-num_epochs = 20000
+num_epochs = 200000
 
 # omit gene_id, gene_name, gene_type
 X = rnaseq_df.iloc[:, 3:].values
@@ -110,7 +118,7 @@ class BetaVAE(nn.Module):
 
     def encode(self, x):
         x = self.encoder(x)
-        mean, log_var = torch.chunk(x, 2, dim=-1)   # log_var = log(std**2)
+        mean, log_var = torch.chunk(x, 2, dim=-1)  # log_var = log(std**2)
         return mean, log_var
 
     def decode(self, z):
@@ -140,13 +148,13 @@ if torch.cuda.device_count() > 1:
     print(f"using {torch.cuda.device_count()} GPUs")
     model = nn.DataParallel(model)
 
-
 for epoch in range(num_epochs):
     model.train()
     train_loss = 0.0
     for data in train_loader:
         optimizer.zero_grad()
         input = data[0].to(device)
+        current_lr = optimizer.param_groups[0]['lr']
         # if epoch == 0:
         #     print("Model summary: ", summary(model, input.shape))
         recon_batch, mean, log_var = model(input)
@@ -157,12 +165,9 @@ for epoch in range(num_epochs):
         optimizer.step()
         train_loss += loss.item()
 
-        wandb.log({"epoch": epoch, "train_loss": train_loss, "reconstruction_loss": reconstruction_loss.item(),
-                   "kl_div_loss": kl_divergence_loss.item()})
-
     if epoch % 100 == 0:
         # validation and print losses
-        current_lr = optimizer.param_groups[0]['lr']
+
         print("epoch: ", epoch, " LR: ", current_lr)
         print("training loss (reconstruction_loss, kl_divergence_loss): ", train_loss, "(", reconstruction_loss, ",",
               kl_divergence_loss, ")")
@@ -175,9 +180,24 @@ for epoch in range(num_epochs):
                 reconstruction_loss, kl_divergence_loss = loss_function(recon_batch, input, mean, log_var)
                 loss = reconstruction_loss + beta * kl_divergence_loss
                 val_loss += loss.item()
+        # set_trace()
         val_loss /= len(val_loader.dataset)
         train_loss /= len(train_loader.dataset)
         print("validation loss: ", val_loss)
+
+    # save model and other states every 10K epochs
+    if epoch % 10000 == 0:
+        checkpoint_path = os.path.join(checkpoint_dir, f"checkpoint_epoch_{epoch}.pth")
+        torch.save({
+            'epoch': epoch,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'scheduler_state_dict': scheduler.state_dict(),
+        }, checkpoint_path)
+        print(f"checkpoint saved at epoch {epoch} to {checkpoint_path}")
+
+    wandb.log({"epoch": epoch, "LR": current_lr, "train_loss": train_loss, "reconstruction_loss": reconstruction_loss.item(),
+               "kl_div_loss": kl_divergence_loss.item(), "val_loss": val_loss})
 
     scheduler.step()
 
