10cc1e195f70da7da18166dbef63eafe13fb3907
tnnandi <tnnandi@gmail.com>
2024-04-08 00:08:50 -0500

    updating data mapping script to write out json file to avoid writing out lists as strings

--- joint_fusion/create_image_molecular_mapping.py
+++ joint_fusion/create_image_molecular_mapping.py
@@ -8,21 +8,23 @@ from pdb import set_trace
 
 base_dir = '/mnt/c/Users/tnandi/Downloads/multimodal_lucid/multimodal_lucid/preprocessing/'
 # path to the directory containing the tiles
-tiles_dir = base_dir + 'data_3/tiles/256px_9.9x/'
+tiles_dir = base_dir + 'TCGA_WSI/batch_corrected/processed_svs/tiles/256px_9.9x/' # contains stain corrected tiles for around 100 TCGA-LUAD samples
 
 # path to the csv file containing the clinical data
 # input_csv_path = '/mnt/c/Users/tnandi/Downloads/multimodal_lucid/multimodal_lucid/preprocessing/combined_clinical_TCGA-LUAD.csv'
-data_clinical_df = pd.read_csv(base_dir + 'combined_clinical_TCGA-LUAD.csv', delimiter='\t')
-data_rnaseq_df = pd.read_csv(base_dir + 'combined_rnaseq_TCGA-LUAD.csv', delimiter='\t')
+data_clinical_df = pd.read_csv(base_dir + 'combined_clinical_TCGA-LUAD.csv', delimiter='\t') # contains [time_to_death, time_to_last_followup, dead/alive status] for all LUAD samples
+data_rnaseq_df = pd.read_csv(base_dir + 'combined_rnaseq_TCGA-LUAD.csv', delimiter='\t') # contains TPM for all protein coding genes for all LUAD samples
 
 # csv file with the mapped data
 output_csv_path = base_dir + './mapped_data.csv'
+# json file with the mapped data
+output_json_path = base_dir + './mapped_data.json'
 
 png_files_dict = {}
 
 for tile_dir in os.listdir(tiles_dir):
     for filename in os.listdir(tiles_dir + tile_dir):
-        if filename.endswith('.png'):
+        if filename.endswith('.png') or filename.endswith('.jpg'):
             tcga_id = filename.split('-')[0] + '-' + filename.split('-')[1] + '-' + filename.split('-')[2] #+ '-' + filename.split('-')[3]
             png_file_name = filename #.rsplit('.', 1)[0]
             # Note: there may be multiple WSIs for a single patient, so that needs to be accounted for
@@ -66,20 +68,12 @@ data_clinical_df = pd.DataFrame({
     'event_occurred': vital_status_list
 }).set_index('sample_id')
 
-
-# Add a column from png_files_dict to the above df
-# Creating a DataFrame with a single column to hold the lists from png_files_dict
 combined_df = data_clinical_df.copy()
 
-# Initialize a new column in combined_df for the file lists
 combined_df['tiles'] = [[] for _ in range(len(combined_df))]
 
-# Populate the 'files' column with data from png_files_dict where available
 for idx in combined_df.index:
     combined_df.at[idx, 'tiles'] = png_files_dict.get(idx, [])
-
-
-set_trace()
 
 # add another columns (that is essentially a dictionary) for the rnaseq data
 print("Do gene_name have repeats: ", data_rnaseq_df['gene_name'].duplicated().any())
@@ -94,8 +88,17 @@ for tcga_id in combined_df.index:
         # create a dictionary for each row with gene_id: expression_value
         combined_df.at[tcga_id, 'rnaseq_data'] = rnaseq_transposed.loc[tcga_id].to_dict()
 
+# combined_df['rnaseq_data'] = None
+# for tcga_id in combined_df.index:
+#     if tcga_id in rnaseq_transposed.index:
+#         combined_df.at[tcga_id, 'rnaseq_data'] = rnaseq_transposed.loc[tcga_id].to_dict()
+#     else:
+#         combined_df.at[tcga_id, 'rnaseq_data'] = {}
+
+# set_trace()
 combined_df.to_csv(output_csv_path)
-print(f"filtered data has been written to {output_csv_path}.")
+combined_df.to_json(output_json_path)
+print(f"filtered data has been written to {output_csv_path} and {output_json_path}.")
 
 set_trace()
 
